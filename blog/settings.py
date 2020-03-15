8c75958cb5f7f7604e9298096ef095dece6961cc
Albena Kr <albena.krasteva@gmail.com>
2020-03-15 10:00:20 +0000

    Updated ALLOWED HOSTS

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://blog-app-test-ci.herokuapp.com/']
+ALLOWED_HOSTS = ['blog-app-test-ci.herokuapp.com']
 
 
 # Application definition
